e54ab5e38279fc6a283cf481663da727a7f6087a
fionadonohue1228 <152354471+fionadonohue1228@users.noreply.github.com>
2025-11-29 18:17:04 -0500

    Create spotify.js

---
+++ spotify.js
@@ -0,0 +1,57 @@
+const margin = { top: 70, right: 30, bottom: 40, left: 80 };
+const width = 1200 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const x = d3.scaleLinear().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
+
+const svg = d3.select("#chart-container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+d3.csv("15000_tracks_cleaned.csv").then(function(data) {
+
+  data = data.filter(d => +d.year >= 1950 && +d.year <= 2025);
+
+  data.sort((a, b) => +a.year - +b.year);
+
+  x.domain(d3.extent(data, d => +d.year));
+  y.domain([0, d3.max(data, d => +d.duration_ms)]);
+
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 35)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .text("Year");
+
+  svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -50)
+    .style("font-size", "16px")
+    .text("Duration (ms)");
+
+  const line = d3.line()
+    .x(d => x(+d.year))           
+    .y(d => y(+d.duration_ms)); 
+
+  svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "blue")
+    .attr("stroke-width", 2)
+    .attr("d", line);
+
+});
