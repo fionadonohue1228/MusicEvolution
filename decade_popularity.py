86151140d6b7b6b0269c6fee32b4958fdc025ee2
fionadonohue1228 <152354471+fionadonohue1228@users.noreply.github.com>
2025-11-30 20:14:25 -0500

    Update and rename dance_popularity.py to decade_popularity.py

--- decade_popularity.py
+++ decade_popularity.py
@@ -1,22 +1,20 @@
 import altair as alt
 import pandas as pd
 
-spotify_rand = spotify.sample(n=50, random_state=42)
+spotify_rand = spotify.sample(n=100, random_state=42)
 
 slider = alt.binding_range(min=0.05, max=1, step=0.05, name="Popularity:")
 op_var = alt.param(value=0.5, bind=slider)
 
 chart = alt.Chart(spotify_rand).mark_bar().encode(
     x=alt.X('track_name:N', title='Track Name'),
-    y=alt.Y('danceability:Q', title='Danceability'),
-    color=alt.Color('popularity:Q', title='Popularity'),
+    y=alt.Y('popularity:Q', title='Popularity'),
+    color=alt.Color('decade:Q', title='Decade'),
     opacity=alt.OpacityValue(op_var)
 ).add_params(
     op_var
 ).properties(
     width=1000,
     height=500,
-    title='Danceability Popularity by Track'
+    title='Decade Popularity by Track'
 )
-
-
