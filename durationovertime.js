3c2d263f15732ce92535d072f7a7fbaa9dcbcc3e
fionadonohue1228 <152354471+fionadonohue1228@users.noreply.github.com>
2025-11-30 21:53:18 -0500

    Update durationovertime.js

--- durationovertime.js
+++ durationovertime.js
@@ -17,31 +17,25 @@ let svg = d3.select("#chart-container")
 
 d3.csv("15000_tracks_cleaned.csv").then(function(data) {
 
-    // Convert values
     data.forEach(d => {
         d.year = +d.year;
-        d.duration = +d.duration_ms;   // <-- KEEPING MILLISECONDS
+        d.duration = +d.duration_ms;   
     });
 
-    // Keep reasonable years
     data = data.filter(d => d.year >= 1950 && d.year <= 2025);
 
-    // Remove insane outliers (30 sec to 10 min)
     data = data.filter(d => d.duration > 30000 && d.duration < 600000);
 
-    // Sort by year
     data.sort((a, b) => a.year - b.year);
 
-    // ----- SCALES -----
     let yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.duration)])   // ms
+        .domain([0, d3.max(data, d => d.duration)]) 
         .range([height, 0]);
 
     let xScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.year))         // years
+        .domain(d3.extent(data, d => d.year))        
         .range([0, width]);
 
-    // ----- AXES -----
     let xAxis = svg.append("g")
         .call(d3.axisBottom(xScale).tickFormat(d3.format("d")))
         .attr("transform", `translate(0, ${height})`);
@@ -49,7 +43,6 @@ d3.csv("15000_tracks_cleaned.csv").then(function(data) {
     let yAxis = svg.append("g")
         .call(d3.axisLeft(yScale));
 
-    // ----- AXIS LABELS -----
     svg.append("text")
         .attr("x", width / 2)
         .attr("y", height + 40)
@@ -60,12 +53,12 @@ d3.csv("15000_tracks_cleaned.csv").then(function(data) {
     svg.append("text")
         .attr("x", -height / 2)
         .attr("y", -40)
-        .attr("transform", "rotate(-90)")
         .style("text-anchor", "middle")
         .style("font-size", "16px")
         .text("Duration (ms)");
+        .attr("transform", "rotate(-90)")
 
-    // ----- LINE -----
+
     let line = d3.line()
         .x(d => xScale(d.year))
         .y(d => yScale(d.duration));
