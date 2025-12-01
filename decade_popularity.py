417c959afaca52506c9d205e52831ea569dde7e0
fionadonohue1228 <152354471+fionadonohue1228@users.noreply.github.com>
2025-11-30 21:17:07 -0500

    Update decade_popularity.py

--- decade_popularity.py
+++ decade_popularity.py
@@ -6,7 +6,6 @@ spotify_rand = spotify.sample(n=100, random_state=42)
 slider = alt.binding_range(min=0.05, max=1, step=0.05, name="Popularity:")
 op_var = alt.param(value=0.5, bind=slider)
 
-# Build chart
 decade = alt.Chart(spotify_rand).mark_bar().encode(
     x=alt.X('track_name:N', title='Track Name'),
     y=alt.Y('popularity:Q', title='Popularity (0-100)'),
@@ -17,10 +16,11 @@ decade = alt.Chart(spotify_rand).mark_bar().encode(
 ).properties(
     width=1000,
     height=500,
-     title=alt.TitleParams(
-        text='Decade Popularity by Track',
-        subtitle='Showing 100 randomly selected tracks'
-    )
+    title={
+        "text": "Decade Popularity by Track",
+        "subtitle": "Showing 100 randomly selected tracks",
+        "fontSize": 22,
+        "subtitleFontSize": 13
+    }
 )
-decade
 decade
