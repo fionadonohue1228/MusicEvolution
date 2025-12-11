6e400bcf7bf526f6bb3311d29a34305de40b1072
fionadonohue1228 <152354471+fionadonohue1228@users.noreply.github.com>
2025-12-11 14:08:25 -0500

    Update durationovertime.js

--- durationovertime.js
+++ durationovertime.js
@@ -3,106 +3,178 @@ let width = 800,
 
 let margin = {
   top: 50,
-  bottom: 50,
+  bottom: 80,   
   left: 80,
-  right: 50
+  right: 80
 };
 
-let svg = d3.select('body')
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-
+let svg = d3.select("body")
+  .append("svg")
+  .attr("width", width + margin.right + 40)
+  .attr("height", height + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("15000_tracks_cleaned.csv").then(function(data) {
 
-    data.forEach(d => {
-        d.year = +d.year;
-        d.duration = +d.duration_ms / 1000;
-    });
+  data.forEach(d => {
+    d.year = +d.year;
+    d.duration = +d.duration_ms / 1000;
+  });
 
-    data.sort((a, b) => a.year - b.year);
+  let genres = [
+    "acoustic", 
+    "afrobeat", 
+    "ambient", 
+    "bluegrass", 
+    "chicago-house",
+    "children",
+    "club",
+    "comedy"
+  ];
 
+  data = data.filter(d => genres.includes(d.track_genre));
 
-    let year = data.map(d => d.year);
+  data.sort((a, b) => a.year - b.year);
+  let year = data.map(d => d.year);
 
-    let x = d3.scaleLinear()
-        .range([0, width])
-        .domain([0, 600]);
+  let x = d3.scaleLinear()
+    .range([0, width])
+    .domain([0, 600]);
 
-    svg.append("g")
-        .attr("transform", "translate(0," + (height - 80) + ")")   
-        .call(
-            d3.axisBottom(x)
-                .ticks(10)
-                .tickFormat(d => Math.round(d) + "s")
-        );
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - 80) + ")")
+    .call(
+      d3.axisBottom(x)
+        .ticks(10)
+        .tickFormat(d => Math.round(d) + "s")
+    );
 
-  
-    let y = d3.scaleBand()
-        .range([height - 80, 0])  
-        .domain(year)
-        .padding(0.01);
+  let y = d3.scaleBand()
+    .range([height - 80, 0])
+    .domain(year)
+    .padding(0.01);
 
-    svg.append("g")
-        .call(
-            d3.axisLeft(y)
-                .tickValues(year.filter(d => d % 10 === 0))  
-        );
+  svg.append("g")
+    .call(
+      d3.axisLeft(y)
+        .tickValues(year.filter(d => d % 10 === 0))
+    );
 
+  let colorScale = d3.scaleOrdinal()
+    .domain(genres)
+    .range([
+      "#FFCA8E",
+      "#40EDC8",
+      "#F77B87",
+      "#F7941D",
+      "#662D91",
+      "#FF9DF4",
+      "#FDFF94",
+      "#9BDEFF"
+    ]);
 
-svg.selectAll("rect")
+  svg.selectAll(".dot")
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", d => x(d.duration))    
-    .attr("y", d => y(d.year))        
-    .attr("width", 8)                 
+    .attr("class", "dot")
+    .attr("x", d => x(d.duration))
+    .attr("y", d => y(d.year))
+    .attr("width", 8)
     .attr("height", 8)
     .attr("fill", "#ED9E01")
-    .attr("opacity", 0.07);  
+    .attr("opacity", 0.07);
 
 
+let legendSvg = d3.select("#legend");
 
-d3.select("#decade").on("change", function() {
+legendSvg.append("text")
+  .attr("x", 0)
+  .attr("y", 12)
+  .style("font-size", "14px")
+  .style("font-weight", "bold")
+  .text("Features");
 
-  let decade = this.value;
+let legendItems = legendSvg.selectAll(".legend-item")
+  .data(genres)
+  .enter()
+  .append("g")
+  .attr("class", "legend-item")
+  .attr("transform", (d, i) => {
+    let row = Math.floor(i / 4);
+    let col = i % 4;
 
-  if (decade === "all") {
-    svg.selectAll("rect")
-      .attr("opacity", 0.07);  
-    return;
-  }
+    let y;
+    if (row === 0) {
+        y = 0;      
+    } else {
+        y = 18;    
+    }
 
-  let start = +decade;
-  let end = start + 9;
-
-  svg.selectAll("rect")
-    .attr("opacity", d =>
-      d.year >= start && d.year <= end ? 0.9 : 0.05
-    );
+    return `translate(${60 + col * 120}, ${y})`;
 });
 
+legendItems.append("rect")
+  .attr("width", 12)
+  .attr("height", 12)
+  .attr("fill", d => colorScale(d));
 
-svg.append("text")
+legendItems.append("text")
+  .attr("x", 16)
+  .attr("y", 10)
+  .style("font-size", "10px")
+  .text(d => d);
+
+
+
+  d3.select("#decade").on("change", function () {
+
+    let decade = this.value;
+
+    if (decade === "all") {
+      svg.selectAll(".dot")
+        .attr("opacity", 0.07)
+        .attr("fill", "#ED9E01");
+
+      return;
+    }
+
+    let start = decade * 1;
+    let end = start + 9;
+
+    svg.selectAll(".dot")
+      .attr("opacity", function(d) {
+        if (d.year >= start && d.year <= end) {
+          return 0.9;
+        } else {
+          return 0.05;
+        }
+      })
+      .attr("fill", function(d) {
+        if (d.year >= start && d.year <= end) {
+          return colorScale(d.track_genre);
+        } else {
+          return "#ED9E01";
+        }
+      });
+
+  });
+
+
+  svg.append("text")
     .attr("x", width / 2)
-    .attr("y", height - 55)   
+    .attr("y", height - 55)
     .style("text-anchor", "middle")
     .style("font-size", "16px")
     .text("Duration in Seconds");
 
-
-svg.append("text")
+  svg.append("text")
     .attr("x", -height / 2)
-    .attr("y", -40)   
+    .attr("y", -40)
     .attr("transform", "rotate(-90)")
     .style("text-anchor", "middle")
     .style("font-size", "16px")
     .text("Decade");
-
 
 });
